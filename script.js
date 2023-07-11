647f0ba2d4061f20874b715e0f6a3fe1a4cb8b19
Parisa <drpm52@gmail.com>
2023-07-10 19:11:19 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// color example:
+// /id{?hex,rgb,hsl,cmyk,format}
+// example URI 
+// GET https://www.thecolorapi.com/id?hex=0047AB&rgb=0,71,171&hsl=215,100%,34%&cmyk=100,58,0,33&format=html
+// generate schemes
+// /scheme{?hex,rgb,hsl,cmyk,format,mode,count}
+
+// /scheme?hex=24B1E0&mode=triad&count=6 || /scheme?rgb=rgb(0,71,171) || ...
+// example URI for getting schemes
+https://www.thecolorapi.com/scheme?hex=0047AB&rgb=0,71,171&hsl=215,100%,34%&cmyk=100,58,0,33&format=html&mode=analogic&count=6
+
+
